5597293482ea032bb96ae6e028a755279c852e75
Christoffer <clyhol20@student.aau.dk>
2024-09-23 13:39:27 +0200

    working plot generation

--- generate_plots.py
+++ generate_plots.py
@@ -1,9 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from load_files import load_data
+import os
 
-data = load_data("test")
-def generate_plots(data):
+def generate_plots(data, directory=""):
+    """This function takes a list of data of form ['freqs', 'snr', 'symbol'] and generated binary FFT modulus plots.
+    
+        Use load_data from load_data.py to get data from csv to dataframe of this form
+
+    Args:
+        data (_type_): list containing absolute values of the generated FFT
+        directory (str, optional): Directory to where the plots folder should be created. Defaults to current working directory.
+    """    
+    sample_idx = 0
+    
     for i in range(len(data)):
         freqs = list(map(float, data["freqs"].iloc[i].split(';'))) # split string and convert to float
         freqs_idx = np.arange(0, len(freqs), 1)
@@ -14,50 +24,22 @@ def generate_plots(data):
         fig.set_facecolor('black')
         plt.plot(freqs_idx, freqs, color = 'white', linewidth=1)
         plt.close(fig)
-        fig.savefig(f"data/snr_{data['snr'].iloc[i]}_symbol_{data['symbol'].iloc[i]}.png", bbox_inches='tight', pad_inches=0)
+        
+        # this counter is used to create a unique name for each plot
+        sample_idx += 1
+        if sample_idx == 1001: # hard coded as we have 1000 representations
+            sample_idx = 1
+        
+        # create plots folder in given directory
+        plots_dir = os.path.join(directory, "plots")
+        os.makedirs(plots_dir, exist_ok=True)
+        
+        #save images to folder
+        fig.savefig(os.path.join(plots_dir, f"snr_{data['snr'].iloc[i]}_symbol_{data['symbol'].iloc[i]}_{sample_idx}.png"), bbox_inches='tight', pad_inches=0)
 
 
 if __name__ == "__main__":
-    
-    # # create fft plot
-    # plt.figure()
-    # plt.plot(freqs_idx, freqs)
-    # plt.title("FFT")
-    # plt.xlabel("Frequency")
-    # plt.ylabel("Magnitude")
-    # plt.show()
-    pass
-
-
-# import numpy as np
-# import matplotlib.pyplot as plt
-# from multiprocessing import Pool
-# import os
-
-# def save_plot(args):
-#     freqs, freqs_idx, snr, symbol, i = args
-#     fig = plt.figure(figsize=(2, 2))
-#     plt.axis('off')
-#     fig.set_facecolor('black')
-#     plt.plot(freqs_idx, freqs, color='white', linewidth=1)
-#     fig.savefig(f"data/snr_{snr}_symbol_{symbol}_{i}.png", bbox_inches='tight', pad_inches=0)
-#     plt.close(fig)
-
-# if __name__ == "__main__":
-#     # Load data
-#     data = load_data("test")
-
-#     # Prepare arguments for multiprocessing
-#     args_list = []
-#     for i in range(len(data)):
-#         freqs = list(map(float, data["freqs"].iloc[i].split(';')))  # split string and convert to float
-#         freqs_idx = np.arange(0, len(freqs), 1)
-#         snr = data["snr"].iloc[i]
-#         symbol = data["symbol"].iloc[i]
-#         args_list.append((freqs, freqs_idx, snr, symbol, i))
-
-#     # Use multiprocessing to save plots in parallel
-#     with Pool(os.cpu_count()) as pool:
-#         pool.map(save_plot, args_list)
+    data = load_data("test_data_for_plots")
+    generate_plots(data)
     
     
