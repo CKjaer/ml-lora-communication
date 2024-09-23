06cbb87d0b0da7dae02755b812c83aff37c898a6
Christoffer <clyhol20@student.aau.dk>
2024-09-23 20:59:53 +0200

    Fixed image size - linewidth 0.5 - resolution 128x128

--- plot_stuff/generate_plots.py
+++ plot_stuff/generate_plots.py
@@ -19,12 +19,13 @@ def generate_plots(data, directory=""):
     for i in range(len(data)):
         freqs = list(map(float, data["freqs"].iloc[i].split(';'))) # split string and convert to float
         freqs_idx = np.arange(0, len(freqs), 1)
-        print(data["symbol"].iloc[i])
+        #print(data["symbol"].iloc[i])
 
-        fig = plt.figure(figsize=(2,2))
+        fig = plt.figure(figsize=(1,1), dpi=128)
+        ax = fig.add_subplot(111)
         plt.axis('off')
         fig.set_facecolor('black')
-        plt.plot(freqs_idx, freqs, color = 'white', linewidth=1)
+        plt.plot(freqs_idx, freqs, color = 'white', linewidth=0.5)
         plt.close(fig)
         
         # this counter is used to create a unique name for each plot
@@ -37,8 +38,9 @@ def generate_plots(data, directory=""):
         os.makedirs(plots_dir, exist_ok=True)
         
         #save images to folder
-        fig.savefig(os.path.join(plots_dir, f"snr_{data['snr'].iloc[i]}_symbol_{data['symbol'].iloc[i]}_{sample_idx}.png"), bbox_inches='tight', pad_inches=0)
-        resize_plot(128, directory=plots_dir)
+        fig.subplots_adjust(left=0, right=1, top=1, bottom=0)
+        fig.savefig(os.path.join(plots_dir, f"snr_{data['snr'].iloc[i]}_symbol_{data['symbol'].iloc[i]}_{sample_idx}.png"), dpi=128)
+        # resize_plot(128, directory=plots_dir)
 
 def resize_plot(num_symbols, directory="plots"):
     """this function resizes the plots to MxM where M is the number of symbols
@@ -54,7 +56,7 @@ def resize_plot(num_symbols, directory="plots"):
             img.save(file_path)
 
 if __name__ == "__main__":
-    data = load_data("test_data_for_plots")
+    data = load_data("test")
     generate_plots(data)
     
     
