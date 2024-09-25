f7384e40c2cb0ff711a4cd0a67b01e125eacf866
Dyb <rdybkj21@student.aau.dk>
2024-09-25 14:45:53 +0200

    renamed main, added parameters

--- training_sims/data_generator/generate_training_data.py
+++ training_sims/data_generator/generate_training_data.py
@@ -1,60 +1,13 @@
-
-import sys
 import os
-file_path = os.path.dirname(__file__)
-include_dir = os.path.abspath(os.path.join(file_path, '../include'))
-sys.path.append(include_dir)
-
 import time
 from numpy import savetxt
-
 import lora_phy as lora
-from json_handler import json_handler
 import tensorflow as tf
 import argparse
+import json
+import logging
 
-
-def main():
-    for i in range(10): print("\n")
-    # Setup of the argument parser. Eases multiple runs of the program
-    desc = (
-        "This program generates test data for the LoRa PHY model.\n"
-        "The data is saved as CSV files in the specified output directory.\n"
-        "The program requires a setup file in JSON format to run.\n"
-        "Note that the program overwrites any existing files with the same name.\n"
-    )
-    parser = argparse.ArgumentParser(prog="LoRa Phy gen",description=desc)
-    parser.add_argument("-c","--config_file", help="A json file containing the setup for the process. The setup file should contain the following fields: 'test_id', 'number_of_samples', 'snr_values' and 'spreading_factor'.", type=str,required=True)
-    parser.add_argument("-o","--output_dir", help=f"Allows for a specified outputdir. Otherwise the default directory is the name of the config file",default="DEFAULT",type=str,required=False)
-    parser.add_argument("-v","--verbose", help="Allows printing of data",action="store_true",default=False)
-
-    args = parser.parse_args()
-    config_file = args.config_file
-    output_dir = args.output_dir
-    verbose = args.verbose
-    
-    # Load the setup file
-    try:
-        setup_path = os.path.abspath(os.path.join(os.getcwd(), config_file))
-        print(setup_path)
-        setup_file = json_handler(setup_path)
-    except FileNotFoundError:
-        print(f"File {config_file} not found. Check the path and try again.")
-        exit()
-
-    # Loads and print the setup data
-    setup_file.print_setup_data()
-
-    N_samp_raw = setup_file.number_of_samples()
-    snr_values_raw = setup_file.get_snr_values()
-    SF_raw = setup_file.get_spreading_factor()
-
-    #Setup file structure for saving the results
-    if output_dir == "DEFAULT":
-        output_dir = setup_file.get_test_id()
-    og_path = os.path.abspath(os.path.join(file_path, "output",output_dir))
-    os.makedirs(og_path, exist_ok=True)
-
+def create_data_csvs(log:logging, N_samples:int, snr_values:int, SF:int, output_dir:str, lamb:float):
     # Check if GPU is available - if it is, tensor flow runs on the GPU
     gpus = tf.config.list_physical_devices('GPU')
     if gpus:
@@ -94,7 +47,7 @@ def main():
         
         for i, snr in enumerate(snr_values):
             tf_snr = tf.constant(snr, dtype=tf.int32)
-            if verbose: print(f"Processing SNR {snr} ({i + 1}/{len(snr_values)})")
+            log.debug(f"Processing SNR {snr} ({i + 1}/{len(snr_values)})")
 
             for j in tf.range(M):
                 # Setup - Start the timer - mostly for fun
@@ -107,9 +60,52 @@ def main():
                 savetxt(file_name, fft_result, delimiter=',')
 
                 end_time = time.time()
-                if verbose: print(f"Processed symbol {j}/{M} for SNR {snr} in {end_time - beginning_time:.4f} seconds - Total: {j+i*M}/{M*len(snr_values)}")
-
-        if verbose: print(f"Total processing taken: {time.time() - start_time}")
+                log.debug(f"\tProcessed symbol {j}/{M} for SNR {snr} in {end_time - beginning_time:.4f} seconds - Total: {j+i*M}/{M*len(snr_values)}")
+        log.debug(f"Total processing taken: {time.time() - start_time}")
 
 if __name__ == '__main__':
-    main()
+    for i in range(10): print("\n")
+    # Setup of the argument parser. Eases multiple runs of the program
+    desc = (
+        "This program generates test data for the LoRa PHY model.\n"
+        "The data is saved as CSV files in the specified output directory.\n"
+        "The program requires a setup file in JSON format to run.\n"
+        "Note that the program overwrites any existing files with the same name.\n"
+    )
+    parser = argparse.ArgumentParser(prog="LoRa Phy gen",description=desc)
+    parser.add_argument("-c","--config_file", help="A json file containing the setup for the process. The setup file should contain the following fields: 'test_id', 'number_of_samples', 'snr_values' and 'spreading_factor'.", type=str,required=True)
+    parser.add_argument("-o","--output_dir", help=f"Allows for a specified outputdir. Otherwise the default directory is the name of the config file",default="DEFAULT",type=str,required=False)
+    parser.add_argument("-v","--verbose", help="Allows printing of data",action="store_true",default=False)
+
+    args = parser.parse_args()
+    config_file = args.config_file
+    output_dir = args.output_dir
+    verbose = args.verbose
+    
+    logfilename = "test.log"
+    file_path = os.path.dirname(os.path.realpath(__file__))
+    log_path = os.path.abspath(os.path.join(file_path,logfilename))
+    logger = logging.getLogger("LoRa Phy gen")
+    logging.basicConfig(filename=log_path, encoding='utf-8', level=logging.DEBUG)
+    logger.debug("Starting the program")
+    # Load the setup file
+    try:
+        setup_path = os.path.abspath(os.path.join(os.getcwd(), config_file))
+        logger.debug(setup_path)
+        setup_json = json.load(open(setup_path))
+    except FileNotFoundError:
+        logger.error(logger,f"File {config_file} not found. Check the path and try again.")
+        exit()
+
+    # Loads and print the setup data
+    N_samp_raw = setup_json.get("number_of_samples")
+    snr_values_raw = setup_json.get("snr_values")
+    SF_raw = setup_json.get("spreading_factor")
+
+    #Setup file structure for saving the results
+    if output_dir == "DEFAULT":
+        output_dir = setup_json.get("test_id")
+    og_path = os.path.abspath(os.path.join(file_path, "output",output_dir))
+    os.makedirs(og_path, exist_ok=True)
+
+    create_data_csvs(logger, N_samp_raw, snr_values_raw, SF_raw, og_path,0)
