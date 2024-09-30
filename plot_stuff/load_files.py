def4ab63037d77e020c947389cfac615258380e9
Christoffer <clyhol20@student.aau.dk>
2024-09-30 15:20:05 +0200

    Moved string splitting to separate function using numpy

--- plot_stuff/load_files.py
+++ plot_stuff/load_files.py
@@ -41,7 +41,9 @@ def load_data(directory, logger):
     # Concatenate all dataframes into one large dataframe
     combined_data = pd.concat(data_list, ignore_index=True)
     combined_data.rename(columns={0: "freqs"}, inplace=True)
-    combined_data['freqs'] = combined_data['freqs'].apply(lambda x: list(map(float, x.split(';'))))
+    print("HERE")
+    combined_data = split_freqs_column(combined_data)
+    print("Done")
     
     # Sort the data by SNR and Symbol
     combined_data.sort_values(by=['snr','symbol'], ascending=True, inplace=True)
@@ -51,6 +53,18 @@ def load_data(directory, logger):
 
     logger.info(f"Loaded {len(combined_data)} samples from {len(data_list)} files in {time.time() - start_time:.4f} seconds")
     return combined_data
+
+def split_freqs_column(df):
+    # Initialize an empty list to store the split frequency lists
+    split_freqs = []
+
+    # Iterate over the 'freqs' column and split the strings
+    for freqs in df['freqs']:
+        split_freqs.append(list(map(float, freqs.split(';'))))
+
+    # Convert the list of lists to a numpy array for better memory management
+    df['freqs'] = np.array(split_freqs, dtype=object)
+    return df
 
 if __name__ == "__main__":
     ...
