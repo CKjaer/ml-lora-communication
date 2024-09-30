89e62bc62728b4c56b861351a2c4f14a14902b89
CKjaer <ChristianKjaerNielsen@gmail.com>
2024-09-30 11:36:09 +0200

    added interfering users to batch process

--- training_sims/data_generator/lora_phy.py
+++ training_sims/data_generator/lora_phy.py
@@ -1,10 +1,11 @@
 import tensorflow as tf
 from math import pi
 
+
 @tf.function
-def create_basechirp(M,device):
+def create_basechirp(M, device):
     """Create the basic LoRa chirp
-     
+
     Formula based on "Efficient Design of Chirp Spread Spectrum Modulation for Low-Power Wide-Area Networks" by Nguyen et al.
     The base chirp is calculated as exp(2*pi*1j*((n^2/(2*M)) - (n/2))).
 
@@ -12,19 +13,20 @@ def create_basechirp(M,device):
         tf.complex64: A [M] tensor of complex64 values representing the basic chirp
     """
     with device:
-        n = tf.linspace(0, M-1, M)
+        n = tf.linspace(0, M - 1, M)
         n = tf.cast(n, tf.complex64)
         M_complex = tf.cast(M, tf.complex64)
 
-        #We've split the calculation into two parts - mult1 handling 2*pi*1j and mult2 handling the rest of the formula
-        mult1 = tf.constant(2*pi*1j, tf.complex64)
+        # We've split the calculation into two parts - mult1 handling 2*pi*1j and mult2 handling the rest of the formula
+        mult1 = tf.constant(2 * pi * 1j, tf.complex64)
 
         # Mult 2 is calculated as (n^2/(2*M)) - (n/2)
-        frac1 = tf.divide(tf.pow(n,2), (2*M_complex))
-        frac2 = tf.divide(n,2)
-        mult2 = tf.subtract(frac1,frac2)
+        frac1 = tf.divide(tf.pow(n, 2), (2 * M_complex))
+        frac2 = tf.divide(n, 2)
+        mult2 = tf.subtract(frac1, frac2)
         return tf.exp(tf.multiply(mult1, mult2))
-    
+
+
 @tf.function
 def upchirp_lut(M, basic_chirp, device):
     """Create a lookup table based on the basic chirp.
@@ -35,11 +37,13 @@ def upchirp_lut(M, basic_chirp, device):
         # Sets up a tensor array with datatype of the basic chirp and size of M
         lut_array = tf.TensorArray(dtype=basic_chirp.dtype, size=M, dynamic_size=False)
         lut_array = lut_array.write(0, basic_chirp)  # Write the basic chirp
-        
-        for i in tf.range(1,M):
+
+        for i in tf.range(1, M):
             rolled_chirp = tf.roll(basic_chirp, -i, axis=0)
             lut_array = lut_array.write(i, rolled_chirp)
+
         return lut_array.stack()
+
 
 @tf.function
 def channel_model(SNR, signal_length, M, device):
@@ -49,25 +53,15 @@ def channel_model(SNR, signal_length, M, device):
     """
     with device:
         SNR_complex = tf.cast(SNR, tf.complex64)
-        noise_power = 1/tf.pow(tf.constant(10.0,tf.complex64),SNR_complex/10.0)
+        noise_power = 1 / tf.pow(tf.constant(10.0, tf.complex64), SNR_complex / 10.0)
 
-        tfr1 = tf.random.normal((signal_length, M),dtype=tf.float32)
-        tfr2 = tf.random.normal((signal_length, M),dtype=tf.float32)
-        noise_complex = tf.complex(tfr1,tfr2)
-        noise = noise_complex*tf.sqrt(noise_power/2.0)
+        tfr1 = tf.random.normal((signal_length, M), dtype=tf.float32)
+        tfr2 = tf.random.normal((signal_length, M), dtype=tf.float32)
+        noise_complex = tf.complex(tfr1, tfr2)
+        noise = noise_complex * tf.sqrt(noise_power / 2.0)
         return noise
 
-# @tf.function
-# def channel_model_new(signal_length, M, device):
-#      """A circular AWGN channel with a fixed noise power and 
-#     Returns:
-#         tf.complex64: A [signal_length, M] tensor of complex64 values representing the noise and intefering users
-#     """
-#     with device:
-        
 
-#         return 
-    
 @tf.function
 def generate_inteference_lut(lut, M, n_users, device):
     """Generates the interference table for the LoRa PHY.
@@ -80,8 +74,58 @@ def generate_inteference_lut(lut, M, n_users, device):
         int_vec = tf.squeeze(int_vec)
         symbol1 = tf.gather(lut, int_vec[0, :], axis=0)
         symbol2 = tf.gather(lut, int_vec[1, :], axis=0)
-   
+
         # interference_table = tf.multiply(symbol1, symbol2)
-        # symbol2 = tf.gather(lut, int_vec, axis=1)
+        # symbol2 = tf.gather(lut, int_vec, axis=1)'
 
         return 0
+
+
+@tf.function
+def generate_interferer_symbols(batch_size, rate_param, M, upchirp, device):
+    """
+    Generate symbols for interferers based on a Poisson distribution.
+
+    This function draws a number of interferers from a Poisson distribution, generates random symbols for
+    each interferer, and shifts them with a random arrival time.
+
+    Args:
+        batch_size (int): The number of batches to process.
+        rate_param (float): The rate parameter for the Poisson distribution.
+        M (int): The maximum value for random symbols and a reference for masking.
+        upchirp (tf.Tensor): A tensor representing the look-up table (LUT) for symbols.
+
+    Returns:
+        tf.Tensor: A tensor containing the shifted complex symbols for each batch.
+    """
+    # Draw the number of interferers from a Poisson distribution
+    n_interferers = tf.random.poisson((batch_size,), rate_param, dtype=tf.int32)
+
+    # Create 2 random symbols for each interferer
+    max_interferers = tf.reduce_max(n_interferers).numpy()
+    rand_inter_symbols = tf.random.uniform(
+        (batch_size, max_interferers), minval=0, maxval=M, dtype=tf.int32
+    )
+
+    # Create a mask to zero out the symbols that are not used if the number of interferers is less than the max
+    mask = tf.sequence_mask(n_interferers, max_interferers, dtype=tf.int32)
+
+    # Fill the masked symbols with 128 pointing to a zero symbol in the LUT
+    masked_symbols = tf.where(
+        (rand_inter_symbols * mask) == 0, M, (rand_inter_symbols * mask)
+    )
+
+    # Gather the symbols from the LUT and reshape to stack symbols for each interferer column-wise
+    inter_symbols_lut = tf.gather(upchirp, masked_symbols, axis=0)
+    shaped_symbols = tf.reshape(inter_symbols_lut, (batch_size, 256))
+
+    # Shift the symbols with a random arrival time
+    rand_arrival = tf.random.uniform((batch_size,), minval=0, maxval=M, dtype=tf.int32)
+    shifted_inter = tf.roll(shaped_symbols, shift=-rand_arrival, axis=1)
+
+    # Slice to a singular symbol per interferer
+    num_cols = range(max_interferers * M)
+    # sorted_slice = tf.sort(tf.concat([num_cols[0::M], num_cols[1::M]], axis=0))
+    sliced_inter = tf.gather(shifted_inter, [num_cols], axis=1)
+
+    return sliced_inter
--- training_sims/data_generator/simulate_SNR_values.py
+++ training_sims/data_generator/simulate_SNR_values.py
@@ -7,137 +7,156 @@ from numpy import savetxt
 import os
 import tensorflow as tf
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Check if GPU is available - if it is, tensor flow runs on the GPU
     # Otherwise, run it on a CPU :)
-    device = tf.device('')
-    gpus = tf.config.list_physical_devices('GPU')
+    device = tf.device("")
+    gpus = tf.config.list_physical_devices("GPU")
     if gpus:
-        print('Found GPU, using that')
-        device = tf.device('/device:GPU:0')
+        print("Found GPU, using that")
+        device = tf.device("/device:GPU:0")
     else:
-        print('GPU device not found, using CPU')
-        device = tf.device('/device:CPU:0')
-
+        print("GPU device not found, using CPU")
+        device = tf.device("/device:CPU:0")
 
     with device:
-        # LoRa PHY parameters 
-        SF = 7 # Spreading factor 
-        BW = 250e3 # Bandwidth [Hz] (EU863-870 DR0 channel)
-        M = int(2**SF) # Number of symbols per chirp
-        
+        # LoRa PHY parameters value
+        SF = 7  # Spreading factor
+        BW = 250e3  # Bandwidth [Hz] (EU863-870 DR0 channel)
+        M = int(2**SF)  # Number of symbols per chirp
+
         # Create the basic chirp - Formula based on "Efficient Design of Chirp Spread Spectrum Modulation for Low-Power Wide-Area Networks" by Nguyen et al.
         basic_chirp = lora.create_basechirp(M, device)
         # Once the basic chirp is created, we can create the upchirp LUT, as it is faster than calculating it on the fly
-        upchirp = lora.upchirp_lut(M, basic_chirp, device)
+        upchirp = tf.concat(
+            [
+                lora.upchirp_lut(M, basic_chirp, device),
+                tf.zeros((1, M), dtype=tf.complex64),
+            ],
+            axis=0,
+        )
+
         # A dechirp is simply the conjugate of the upchirp - predefined to make the dechirping operation faster
         basic_dechirp = tf.math.conj(basic_chirp)
 
         # Simulation parameters
-        N = int(1e7) # Number of symbols to simulate is greater than 1e-5 SER for better accuracy
-        batch_size = int(1e6) # Number of symbols per batch
-        nr_of_batches = int(N/batch_size) # NB: N must be divisible by batch_size
+        N = int(
+            1e7
+        )  # Number of symbols to simulate is greater than 1e-5 SER for better accuracy
+        batch_size = int(1e6)  # Number of symbols per batch
+        nr_of_batches = int(N / batch_size)  # NB: N must be divisible by batch_size
 
-        snr_values = tf.linspace(-16, -4, 13) # SNR values to test
+        snr_values = tf.linspace(-16, -4, 13)  # SNR values to test
         snr_values = tf.reverse(snr_values, axis=[0])
-        result_list = [0]*len(snr_values)
+        result_list = [0] * len(snr_values)
 
-        inner_radius = 200 # Inner radius of the circular uniform distribution with no interfering users
-        outer_radius = 1000 # Outer radius of the circular uniform distribution
-        rate_params = [0.25, 0.5, 0.7, 1] # Poisson rate parameters
-
-        # Sample the number of interfering users for each symbol from a Poisson distribution
-        # # Sample the symbol of the interfering users (should be combined)
-        # rand_inter_symbol1 = tf.random.uniform((tf.size(n_interferers),), minval=0, maxval=M, dtype=tf.int32)
-        # rand_inter_symbol2 = tf.random.uniform((tf.size(n_interferers),), minval=0, maxval=M, dtype=tf.int32)
-
-        # # Look up the symbols in the LUT
-        # inter_lut_symbol1 = tf.gather(upchirp, rand_inter_symbol1, axis=0) 
-        # inter_lut_symbol2 =  tf.gather(upchirp, rand_inter_symbol2, axis=0)
-        
-        # symb = tf.concat([inter_lut_symbol1, inter_lut_symbol2], 1)
-        
-        # rand_arrival = tf.random.uniform((10,), minval=0, maxval=5, dtype=tf.int32)
-        # shifted_tensors = tf.roll(symb, shift=-rand_arrival, axis=tf.ones(10, tf.int32))
-        # shifted_tensors = shifted_tensors[:, :128]
-
-        # More efficient way to do the above
+        inner_radius = 200  # Inner radius of the circular uniform distribution with no interfering users
+        outer_radius = 1000  # Outer radius of the circular uniform distribution
+        rate_params = [0.25, 0.5, 0.7, 1]  # Poisson rate parameters
+        k_b = tf.constant(1.380649e-23, dtype=tf.float64)  # Boltzmann constant
 
         @tf.function
         def process_batch(snr, rate_param):
-                
-            n_interferers = tf.random.poisson((batch_size,), rate_params[0], dtype=tf.int32)
-        # Create an array with 0 and 1 if the number of interferers is greater than 1       
-            max_interferers = tf.reduce_max(n_interferers)  
-            rand_inter_symbols = tf.random.uniform((tf.size(n_interferers), max_interferers), minval=0, maxval=M, dtype=tf.int32)
-            
-            inter_symbols_lut = tf.gather(upchirp, rand_inter_symbols, axis=0)  
-            inter_symbols_lut = tf.concat([inter_symbols_lut[:, i, :] for i in range(inter_symbols_lut.shape[1])], axis=1) 
+            # Symbols for interfering users
 
-            rand_arrival = tf.random.uniform((batch_size,), minval=0, maxval=M, dtype=tf.int32)
-           
-            shifted_inter = tf.roll(inter_symbols_lut, shift=-rand_arrival, axis=tf.ones(10, tf.int32))         
-            sliced_tensors = [shifted_inter[:, i:i + M] for i in range(0, shifted_inter.shape[1], 2*M)]
+            # Draw the number of interferers from a Poisson distribution
+            n_interferers = tf.random.poisson(
+                (batch_size,), rate_params[0], dtype=tf.int32
+            )
 
-            # Map the number of interferers to a random radius
-            # random_radii = tf.sqrt(tf.random.uniform((n_interferers, max_interferers), minval=inner_radius, maxval=outer_radius,  dtype=tf.float32))
+            # Create a 2 random symbols for each interferer
+            max_interferers = tf.reduce_max(n_interferers).numpy()
+            rand_inter_symbols = tf.random.uniform(
+                (batch_size, max_interferers), minval=0, maxval=M, dtype=tf.int32
+            )
 
-            k_b = tf.constant(1.380649e-23, dtype=tf.float64) # Boltzmann constant
-            user_power = snr + 10*tf.math.log10(k_b * 298.16 * BW)
-            noise_power = tf.pow(10.0, snr/10.0)
-            
-            # Circular AWGN channel noise 
-            real_part = tf.random.normal(shape=(batch_size,), mean=0.0, stddev=tf.sqrt(noise_power / 2))
-            imaginary_part = tf.random.normal(shape=(batch_size,), mean=0.0, stddev=tf.sqrt(noise_power / 2))
-            
-            # t =  tf.cast(n_interferers > 1, tf.int32)
+            # Create a mask to zero out the symbols that are not used if the number of interferers is less than the max
+            mask = tf.sequence_mask(n_interferers, max_interferers, dtype=tf.int32)
 
-            # array multiplication ones times t
+            # Fill the masked symbols with 128 pointing to a zero symbol in the LUT
+            masked_symbols = tf.where(
+                (rand_inter_symbols * mask) == 0, M, (rand_inter_symbols * mask)
+            )
 
-            msg_tx = tf.random.uniform((batch_size,), minval=0, maxval=M, dtype=tf.int32) # Create an array of random symbols
+            # Gather the symbols from the LUT and reshape to stack symbols for each interferer column-wise
+            inter_symbols_lut = tf.gather(upchirp, masked_symbols, axis=0)
+            shaped_symbols = tf.reshape(inter_symbols_lut, (batch_size, 256))
 
-            uptable = tf.squeeze(tf.gather(upchirp, msg_tx, axis=0))            
+            # Shift the symbols to with a random arrival time
+            rand_arrival = tf.random.uniform(
+                (batch_size,), minval=0, maxval=M, dtype=tf.int32
+            )
+            shifted_inter = tf.roll(
+                shaped_symbols, shift=-rand_arrival, axis=tf.ones(batch_size, tf.int32)
+            )
 
-        
-            modulated_signal = user_power * uptable + tf.reduce_sum(sliced_tensors, axis=1) + tf.sqrt()
+            # Slice to a singular symbol per interferer
+            num_cols = range(max_interferers * M)
+            # sorted_slice = tf.sort(tf.concat([num_cols[0::M], num_cols[1::M]], axis=0))
+            sliced_tensors = tf.gather(shifted_inter, [num_cols], axis=1)
+
+            user_power = snr + 10 * tf.math.log10(k_b * 298.16 * BW)
+            noise_power = tf.pow(10.0, snr / 10.0)
+
+            # Circular AWGN channel noise
+            real_noise = tf.random.normal(
+                shape=(batch_size,), mean=0.0, stddev=tf.sqrt(noise_power / 2)
+            )
+            img_noise = tf.random.normal(
+                shape=(batch_size,), mean=0.0, stddev=tf.sqrt(noise_power / 2)
+            )
+
+            msg_tx = tf.random.uniform(
+                (batch_size,), minval=0, maxval=M, dtype=tf.int32
+            )  # Create an array of random symbols
+
+            uptable = tf.squeeze(tf.gather(upchirp, msg_tx, axis=0))
+
+            modulated_signal = (
+                user_power * uptable
+                + tf.reduce_sum(sliced_tensors, axis=1)
+                + tf.sqrt(real_)
+            )
             # awgn = lora.channel_model_new(snr, batch_size, M, device)
-            
 
             # upchirp_tx = tf.add(uptable, awgn)
-            #Dechirp by multiplying the upchirp with the basic dechirp
+            # Dechirp by multiplying the upchirp with the basic dechirp
             dechirp_rx = tf.multiply(upchirp_tx, basic_dechirp)
             fft_result = tf.abs(tf.signal.fft(dechirp_rx))
-            #Use model to recognize the message
-            msg_rx = model.detect(fft_result,device)
-            #Calculate the number of errors
+            # Use model to recognize the message
+            msg_rx = model.detect(fft_result, device)
+            # Calculate the number of errors
             msg_tx = tf.squeeze(msg_tx)
             batch_result = tf.math.count_nonzero(msg_tx != msg_rx)
 
             return batch_result
-        
+
         for i, snr in enumerate(snr_values):
             for batch in range(nr_of_batches):
                 beginning_time = time.time()
-                print(f"\tSNR {snr}: Batch {batch+1}/{nr_of_batches}. Total:{(nr_of_batches*i+(batch+1))}/{nr_of_batches*len(snr_values)} batches")
+                print(
+                    f"\tSNR {snr}: Batch {batch+1}/{nr_of_batches}. Total:{(nr_of_batches*i+(batch+1))}/{nr_of_batches*len(snr_values)} batches"
+                )
                 tf_snr = tf.constant(snr)
-                result_list[i] += process_batch(tf_snr)
+                result_list[i] += process_batch(tf_snr, rate_params[i])
                 # Setup - Start the timer - mostly for fun
                 end_time = time.time()
                 print(f"\t\tBatch time: {end_time - beginning_time}")
-            print(f'SNR: {snr}dB, error count: {result_list[i]}, SER: {result_list[i]/N:E}')
-        print(f'Time taken: {time.time() - start_time}')
+            print(
+                f"SNR: {snr}dB, error count: {result_list[i]}, SER: {result_list[i]/N:E}"
+            )
+        print(f"Time taken: {time.time() - start_time}")
 
-
-        #Stacks and casts the results to a tensor for saving
-        SF_list = tf.fill([len(snr_values)],tf.cast(SF,tf.float64))
-        N_list = tf.fill([len(snr_values)],tf.cast(N,tf.float64))
+        # Stacks and casts the results to a tensor for saving
+        SF_list = tf.fill([len(snr_values)], tf.cast(SF, tf.float64))
+        N_list = tf.fill([len(snr_values)], tf.cast(N, tf.float64))
         res_list = tf.stack(result_list)
         res_list = tf.cast(res_list, tf.float64)
         snr_list = tf.cast(snr_values, tf.float64)
-        ser_list = tf.divide(res_list,N)
-        output = tf.stack((SF_list, snr_values, res_list, N_list, ser_list),axis=0)
+        ser_list = tf.divide(res_list, N)
+        output = tf.stack((SF_list, snr_values, res_list, N_list, ser_list), axis=0)
 
-        output_path = os.path.abspath(os.path.join(file_path,"output"))
+        output_path = os.path.abspath(os.path.join(file_path, "output"))
         os.makedirs(output_path, exist_ok=True)
         time_str = time.strftime("%Y_%m_%d_%H_%M_%S")
         file_name = f"{output_path}/{time_str}_SER_simulations_results_SF{SF}.txt"
@@ -145,14 +164,14 @@ if __name__ == '__main__':
             f"Test done: {time_str} - "
             f"time taken: {time.time() - start_time} \n"
             f"SF, SNR, Error count, Number of symbols, SER"
-            )
-        savetxt(file_name, output.numpy().T, delimiter=',',header=head)
+        )
+        savetxt(file_name, output.numpy().T, delimiter=",", header=head)
 
-        plt.plot(snr_values, res_list/N,marker='^',linestyle='dashed')
-        plt.yscale('log')
-        plt.xlabel('SNR [dB]')
-        plt.ylabel('SER')
-        plt.legend(['SF'+str(SF)])
-        plt.title('SER vs SNR')
+        plt.plot(snr_values, res_list / N, marker="^", linestyle="dashed")
+        plt.yscale("log")
+        plt.xlabel("SNR [dB]")
+        plt.ylabel("SER")
+        plt.legend(["SF" + str(SF)])
+        plt.title("SER vs SNR")
         plt.savefig(f"{output_path}/{time_str}_SER_simulations_results_SF{SF}.png")
         plt.show()
