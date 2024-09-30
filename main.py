dcbf761d00e23b38475762c9fc31e999c2f7a7b1
Dyb <rdybkj21@student.aau.dk>
2024-09-30 09:28:38 +0200

    Created output folder var

--- main.py
+++ main.py
@@ -12,26 +12,37 @@ if __name__ == "__main__":
     with open('config.json') as f:
         config = json.load(f)
     test_id = str(uuid.uuid4())
-    os.makedirs(os.path.join("output", test_id), exist_ok=True)
-    csv_dir = os.path.join("output", test_id, "csv")
+    output_dir = "output"
+    os.makedirs(os.path.join(output_dir, test_id), exist_ok=True)
+    csv_dir = os.path.join(output_dir, test_id, "csv")
     os.makedirs(csv_dir, exist_ok=True)
 
     # setup logging
     logfilename = "test_log.log"
-    log_path = os.path.join("output",test_id,logfilename)
+    log_path = os.path.join(output_dir,test_id,logfilename)
     logger = logging.getLogger(__name__)
+    print("Logpath: ",log_path)
     logging.basicConfig(filename=log_path, encoding='utf-8', level=logging.DEBUG)
     logger.info("Starting the program")
 
     # generate data
-    create_data_csvs(logger, config["number_of_samples"], config["snr_values"], config["spreading_factor"], csv_dir, config["lambda"])
-    
-    # plot data
-    plot_data = load_data(csv_dir, logger)
-    # generate plots
-    generate_plots(plot_data, logger, config["spreading_factor"], config["number_of_samples"], os.path.join("output", test_id))
+    try:
+        create_data_csvs(logger, config["number_of_samples"], config["snr_values"], config["spreading_factor"], csv_dir, config["lambda"])
+    except Exception as e:
+        logger.error(f"Error generating data: {e}")
+        
+        # plot data
+    try:
+        plot_data = load_data(csv_dir, logger)
+    except Exception as e:
+        logger.error(f"Error loading data: {e}")
+        # generate plots
+    try:
+        generate_plots(plot_data, logger, config["spreading_factor"], config["number_of_samples"], os.path.join(output_dir, test_id))
+    except Exception as e:
+        logger.error(f"Error generating plots: {e}")
     
     # save config
     config["test_id"] = test_id
-    with open(os.path.join("output", test_id, "config.json"), 'w') as f:
+    with open(os.path.join(output_dir, test_id, "config.json"), 'w') as f:
         json.dump(config, f)
