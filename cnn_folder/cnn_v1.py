81613c534db8929c06b351fafa88d2f3cef948ac
Christoffer <clyhol20@student.aau.dk>
2024-10-04 22:06:25 +0200

    Logging test

--- cnn_folder/cnn_v1.py
+++ cnn_folder/cnn_v1.py
@@ -13,8 +13,10 @@ import matplotlib.pyplot as plt
 import logging
 
 # Configure logger
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logfilename = "cnn.log"
 logger = logging.getLogger(__name__)
+logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',datefmt='%Y-%m-%d %H:%M:%S', filename=logfilename, encoding='utf-8', level=logging.INFO)
+logger.info("Starting the program")
 
 
 
