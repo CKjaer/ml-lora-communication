80563e6a4bfeaa6b61557f5bbb3866139a5033d4
xixu64 <mjaure24@studnet.aau.dk>
2024-10-16 13:27:54 +0200

    Moved sweep_config.yaml to cnn_folder and updated tune_hyperparameters.py

--- cnn_folder/tune_hyperparameters.py
+++ cnn_folder/tune_hyperparameters.py
@@ -1,4 +1,16 @@
 import wandb
+import yaml
 
-# Initilize Weights & Biases
-wandb.init(project="")
+# Login into W&B
+# API key: f39f5b62dc920f2d392889028e3d672700fecb83
+#wandb.login()
+
+# Load sweep configuation
+with open('cnn_folder/sweep_config.yaml') as file:
+    sweep_config = yaml.safe_load(file)
+print("Sweep_config file loaded")
+
+# Create the sweep in W&B using the configuration loaded from the YAML
+sweep_id = wandb.sweep(sweep_config, project="lora-symbol-detection")
+
+
