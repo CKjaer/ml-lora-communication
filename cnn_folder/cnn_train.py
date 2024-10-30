a37ee2f040c64410ac03ecc33df8d52675590472
Christoffer <clyhol20@student.aau.dk>
2024-10-30 14:19:32 +0100

    Rewrote plotting part to be consistent with classical decoder plots

--- cnn_folder/cnn_train.py
+++ cnn_folder/cnn_train.py
@@ -14,6 +14,8 @@ import logging
 import numpy as np
 import random
 from numpy import savetxt
+from scipy import stats
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
 # Configure logger
 logfilename = "cnn.log"
@@ -267,21 +269,80 @@ for snr in snr_list:
 logger.info("All SER values have been calculated.")
 
 
+# parameters for plotting
+# plt.rcParams['mathtext.fontset'] = 'custom'
+# plt.rcParams['mathtext.rm'] = 'TeX Gyre Pagella'
+# plt.rcParams['font.family'] ='TeX Gyre Pagella'
+fs = 20
+plt.rcParams.update({'font.size': fs})
+
+
 for rate, values in symbol_error_rates.items():
     snr_values = list(map(int, values.keys()))
     ser_values = list(values.values())
-    
     snr_values = sorted(snr_values)
+    
+    if rate == 0:
+        zero_snr_values = snr_values
+        zero_ser_values = ser_values
+
     savetxt(f'snr_vs_ser_rate_{rate}.csv', np.array([snr_values, ser_values]).T, delimiter=';', fmt='%d;%.6f')
     
-    plt.figure(figsize=(10, 6))
-    plt.plot(snr_values, ser_values, marker='o', linestyle='-', color='b')
-    plt.xlabel('SNR')
-    plt.ylabel('Symbol Error Rate (SER)')
-    plt.yscale('log')
-    plt.ylim(1e-5, 1e0)
-    plt.title(f'SNR vs Symbol Error Rate for rate {rate}')
-    plt.grid(True)
+    fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+    ax.plot(
+        zero_snr_values,
+        zero_ser_values,
+        label=f"SF:{7} λ=0.00",
+        linestyle="dashed",
+        color="black",
+        marker="v"
+    )
+    ax.plot(
+        snr_values,
+        ser_values,
+        marker="v",
+        color="black",
+        label=f"SF:{7} λ={rate:.2f}"
+    )
+    ax.set_xlabel('SNR [dB]')
+    ax.set_ylabel('SER')
+    ax.set_yscale('log')
+    ax.set_ylim(1e-5, 1)
+    ax.set_xlim(-16, -4)
+    ax.grid(True, which="both", alpha=0.5)
+    ax.legend(loc='upper right')
+    
+    
+# Create an inset with the Poisson PMF stem plot
+    inset_ax = inset_axes(
+        ax,
+        width="30%",
+        height="40%",
+        loc="lower left",
+        bbox_to_anchor=(0.1, 0.1, 1, 1),
+        bbox_transform=ax.transAxes,
+    )
+    l = np.linspace(0,10,11)
+    poisson_dist = stats.poisson.pmf(l, mu=rate)
+    print(poisson_dist)
+    mask = (poisson_dist >= 0.005)
+    inset_ax.set_title(f"PMF, λ={rate:.2f}", fontsize = (fs - 2))
+    inset_ax.set_xlabel(r"$\mathrm{N_i}$", labelpad=-4, fontsize = (fs - 2))
+    inset_ax.set_xlim([0, 10])
+    inset_ax.set_ylim([0, 0.8])
+    inset_ax.set_yticks([0.0, 0.2, 0.4, 0.6, 0.8])
+
+    stem_inset = inset_ax.stem(
+        l[mask],
+        poisson_dist[mask],
+        basefmt=" ",
+        linefmt="k-",
+    )
+    # Allow clipping of the stem plot
+    for artist in stem_inset.get_children():
+        artist.set_clip_on(False)
+
+    plt.tight_layout()
         
     # Save the plot
     plot_filename = os.path.join(output_folder, f'snr_vs_ser_rate_{rate}.png')
