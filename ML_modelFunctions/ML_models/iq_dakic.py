6c52b60a892fb3524d5ee917522ccb61d2c8411c
CKjaer <ChristianKjaerNielsen@gmail.com>
2024-11-11 09:52:07 +0100

    Updated iq process

--- ML_modelFunctions/ML_models/iq_dakic.py
+++ ML_modelFunctions/ML_models/iq_dakic.py
@@ -1,30 +1,47 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class IQ_cnn(nn.Module):
     def __init__(self, M):
         super(IQ_cnn, self).__init__()
-        
-        # Convolutional layers
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=M//2, kernel_size=(1,7), stride=1, padding=(1,3))
-        self.conv2 = nn.Conv2d(in_channels=M//2, out_channels=M, kernel_size=(1,7), stride=1, padding=(1,3))
-        self.conv3 = nn.Conv2d(in_channels=M, out_channels=M*2, kernel_size=(1,7), stride=1, padding=(1,3))
-        
-        # Pooling layer
-        self.pool = nn.MaxPool2d(kernel_size=(1,2), stride=2)
-        
-        # Dropout layer
-        self.drop=nn.Dropout(0.1)
 
-        self.batchNorm1=nn.BatchNorm2d(M//2)
-        self.batchNorm2=nn.BatchNorm2d(M)
-        self.batchNorm3=nn.BatchNorm2d(M*2)
+        # Convolutional layers
+        self.conv1 = nn.Conv2d(
+            in_channels=1,
+            out_channels=M // 2,
+            kernel_size=(1, 7),
+            stride=1,
+            padding=(1, 3),
+        )
+        self.conv2 = nn.Conv2d(
+            in_channels=M // 2,
+            out_channels=M,
+            kernel_size=(1, 7),
+            stride=1,
+            padding=(1, 3),
+        )
+        self.conv3 = nn.Conv2d(
+            in_channels=M,
+            out_channels=M * 2,
+            kernel_size=(1, 7),
+            stride=1,
+            padding=(1, 3),
+        )
+
+        # Pooling layer
+        self.pool = nn.MaxPool2d(kernel_size=(1, 2), stride=2)
+
+        # Dropout layer
+        self.drop = nn.Dropout(0.1)
+
+        self.batchNorm1 = nn.BatchNorm2d(M // 2)
+        self.batchNorm2 = nn.BatchNorm2d(M)
+        self.batchNorm3 = nn.BatchNorm2d(M * 2)
 
         # Fully connected layers
-        self.fc1 = nn.Linear(M*(M//2), M)  
-        # self.fc2 = nn.Linear(4 * M, 2 * M)
-        # self.fc3 = nn.Linear(2 * M, M)
-        
+        self.fc1 = nn.Linear(M * (M // 2), M)
+
     def forward(self, x):
         # Convolutional layers
         x = F.relu(self.conv1(x))
@@ -37,17 +54,15 @@ class IQ_cnn(nn.Module):
         x = self.batchNorm3(x)
         x = self.pool(x)
         x = self.drop(x)
-        
+
         # Flatten the output from conv layers
         x = x.view(-1, self.num_flat_features(x))
-        
+
         # Fully connected layers
         x = F.relu(self.fc1(x))
-        # x = F.relu(self.fc2(x))
-        # x = self.fc3(x)
-        
+
         return x
-    
+
     def num_flat_features(self, x):
         size = x.size()[1:]  # All dimensions except the batch dimension
         num_features = 1
--- ML_modelFunctions/find_model.py
+++ ML_modelFunctions/find_model.py
@@ -1,33 +1,32 @@
 import os
 import logging
 import sys
+
 script_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.abspath(os.path.join(script_dir, '..')))
+sys.path.append(os.path.abspath(os.path.join(script_dir, "..")))
 from ML_modelFunctions.ML_models.LoRaCNN import LoRaCNN
-from ML_modelFunctions.ML_models.IQ_cnn import IQ_cnn
+from ML_modelFunctions.ML_models.iq_dakic import IQ_cnn
 import torch
 
-def find_model(model:str):
-    ML_model_files=os.listdir(os.path.join(os.path.dirname(os.path.abspath(__file__)),"ML_models"))
-    ML_models=[modeler.replace(".py", "",-1) for modeler in ML_model_files]
-    
+
+def find_model(model: str):
+    ML_model_files = os.listdir(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "ML_models")
+    )
+    ML_models = [modeler.replace(".py", "", -1) for modeler in ML_model_files]
+
     for i in ML_models:
-        if i==model:
+        if i == model:
             return i
 
-if __name__=="__main__":
-    model="LoRaCNN"
+
+if __name__ == "__main__":
+    model = "LoRaCNN"
     print(find_model(model))
-    
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model=getattr(sys.modules[__name__], model)
-    M=2**7
-    
+    model = getattr(sys.modules[__name__], model)
+    M = 2**7
+
     model(M).to(device)
     print(model)
-    
-
-
-
-
-
