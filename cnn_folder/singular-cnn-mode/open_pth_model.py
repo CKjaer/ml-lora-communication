eef802efef50d56fa11b7459df114e107f674cc3
xixu64 <mjaure24@student.aau.dk>
2024-11-13 11:56:57 +0100

    script organization

--- cnn_folder/singular-cnn-mode/open_pth_model.py
+++ cnn_folder/singular-cnn-mode/open_pth_model.py
@@ -3,9 +3,9 @@ import os
 
 current_folder = os.path.dirname(os.path.abspath(__file__))
 
-models_folder = os.path.join(current_folder, 'models')
+models_combined = os.path.join(current_folder, 'opent_pth_model.py')
 # Cargar el archivo y mostrar sus claves
-checkpoint = torch.load(models_folder, map_location='cpu')
+checkpoint = torch.load(current_folder)
 
 if isinstance(checkpoint, dict):
     print("Keys in checkpoint:", checkpoint.keys())
