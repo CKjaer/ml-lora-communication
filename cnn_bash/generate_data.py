2938729d0abf924dc9b969557dc383c57eeb15b9
CKjaer <ChristianKjaerNielsen@gmail.com>
2024-11-21 10:24:31 +0100

    Plot argument for data is in config instead

--- cnn_bash/generate_data.py
+++ cnn_bash/generate_data.py
@@ -1,3 +1,26 @@
+"""
+This script generates symbol data for CNN training and testing, creating .csv files for both IQ data and FFT data.
+Optionally, it can also generate and save FFT plots as .png files.
+Configuration:
+    The script reads a configuration file (cnn_bash/generate_data_config.json) with the following parameters:
+    - number_of_samples: List of the number of samples to generate.
+    - snr_values: List of SNR values to use.
+    - spreading_factor: Spreading factor for the data generation.
+    - rate: Rate parameter for the data generation.
+    - SIR_random: Flag to indicate whether to use random SIR values.
+    - SIR_span_and_steps: Span and steps for the SIR values.
+    - plot_data: Flag to indicate whether to generate plots.
+    - line_plot: Flag to indicate whether to generate line plots.
+Output:
+    - .csv files with IQ and FFT data.
+    - A log file with details of the data generation.
+    - A copy of the configuration file used for data generation.
+    - If plot_data is True, .png plots of the FFT data.
+Logging:
+    The script sets up logging to record the details of the data generation process. The log file is saved in the output directory with the name 'test_log.log'.
+Exceptions:
+    The script handles exceptions during data creation and plot generation, logging the errors and printing error messages to the console.
+"""
 import os
 import sys
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -10,15 +33,7 @@ import logging
 import time
 
 
-def main(plot_data=False):  
-    """
-    Function to generate symbol data for CNN training and testing. Always creates
-    .csv files for both IQ data and FFT data. Optionally creates and saves plots
-    Args:
-        plot_data (bool): Flag to indicate whether to generate and save plots.
-        if the flag is set to True, the .csv are generated as autoscaled FFT
-        .png plots. Default is False.
-    """
+if __name__ == "__main__":
  
     # Open .json config file with directory and parameters
     with open("cnn_bash/generate_data_config.json") as f:
@@ -73,7 +88,7 @@ def main(plot_data=False):
         print(f"Error creating data: {e}")
 
     # Create image plot if the flag is set
-    if plot_data:
+    if config['plot_data']:
         try:
             plot_data = load_data(csv_dir, logger, header="snr")
         except Exception as e:
@@ -100,6 +115,3 @@ def main(plot_data=False):
     config["test_id"] = test_id
     with open(os.path.join(output_dir, test_id, "config.json"), "w") as f:
         json.dump(config, f)
-
-if __name__ == "__main__":
-    main()   
