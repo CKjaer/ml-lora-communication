f7d2c39a983a1129a0fd174521888f9ec47f7f04
Christoffer <clyhol20@student.aau.dk>
2024-11-29 11:29:10 +0100

    updated ressource allocation

--- cnn_bash/test_iq_model.sh
+++ cnn_bash/test_iq_model.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-#SBATCH --job-name=train_models
+#SBATCH --job-name=test_IQ_models
 #SBATCH --output=./job_results/result_%j.out
 #SBATCH --error=./job_results/error_%j.err
 #SBATCH --time=12:00:00
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=16G
+#SBATCH --cpus-per-task=15
+#SBATCH --mem=24G
 
 singularity shell --nv "$(pwd)/pytorch_24.09.sif" << 'EOF'
 BASE_DIR="$(pwd)"
 echo "Currently in directory: $BASE_DIR"
 
 # Run Python file (evalulate_models.py)
-python -u "$BASE_DIR/IQ_cnn/iq_cnn_test.py" --model IQCNN || { echo "Python script failed"; exit 1; }
+python -u "$BASE_DIR/IQ_cnn/iq_cnn_test.py" || { echo "Python script failed"; exit 1; }
 
 # Exit the container shell
 
