4c169f895e9ba9f3ed01207292930b526d208a8e
CKjaer <ChristianKjaerNielsen@gmail.com>
2024-12-04 19:59:59 +0100

    Add data reading function and new SNR test data for mixed test plots

--- simulation_scripts/sim_output/mixed_test_plot_snr.py
+++ simulation_scripts/sim_output/mixed_test_plot_snr.py
@@ -5,6 +5,29 @@ import pandas as pd
 from scipy import stats
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
+
+def read_data(prefix):  
+    data_list = {}
+    for filename in os.listdir(data_folder):
+        if filename.startswith(prefix) and filename.endswith('.csv'):
+            fp = os.path.join(data_folder, filename)
+            rate = filename.split('_')[-1].removeprefix('lam').removesuffix('.csv')  
+            if rate == '1':
+                rate = '1.0'             
+            with open(fp) as f:
+                lines = f.readlines()
+                for line in lines[1:]:  # Skip the first line
+                    sep = line.strip().split(',')
+                    snr = int(sep[0]) 
+                    ser = float(sep[1])  
+
+                    if snr not in data_list:
+                        data_list[snr] = {}
+
+                    data_list[snr][rate] = ser
+                f.close()
+    return pd.DataFrame.from_dict(data_list, orient='index').reset_index()
+
 if __name__ == "__main__":
         plt.rcParams['mathtext.fontset'] = 'custom'
         plt.rcParams['mathtext.rm'] = 'Palatino Linotype'
@@ -12,58 +35,52 @@ if __name__ == "__main__":
         fs = 20
         plt.rcParams.update({'font.size': fs})
 
-
         current_file_dir = os.path.dirname(os.path.abspath(__file__))
-        data_folder = os.path.join(current_file_dir, "test_data/mixed_test_snr_data")
+        data_folder = os.path.join(current_file_dir, "test_data", "mixed_test_snr_data")
         
-        snr_vals = pd.Series([-16, -14, -12, -10, -8, -6])
-        
-        # lambda = 0.25
-        ser_lam_025 = pd.DataFrame({
-            '0.25': [0.7154017857142857, 0.5066964285714286, 0.2689732142857143, 
-                0.08002804487179487, 0.030899439102564104, 0.01676007264464926]
-        })
 
-        # lambda = 0.5
-        ser_lam_05 = pd.DataFrame({ 
-            '0.5' : [0.7444196428571429, 0.5647321428571429, 0.30691964285714285, 
-                               0.15915464743589744, 0.07868589743589743, 0.045240895417306706]
-        })
+        # Load data for -6 dB mixed test
+        ser_df_6 = read_data('mixed_test_LoRaCNN_snr_-6')  
 
-        # lambda = 0.7       
-        file_path = os.path.join(data_folder, 'mixed_test_LoRaCNN_snr_-10_rate_0.7.csv')
-        ser_lam_07 = pd.read_csv(file_path, sep=',')
+        ser_lam_025 = np.array([0.7154017857142857, 0.5066964285714286, 0.2689732142857143, 
+                0.08002804487179487, 0.030899439102564104, 0.01676007264464926])
 
-        # lambda = 1
-        file_path = os.path.join(data_folder, 'mixed_test_LoRaCNN_snr_-10_rate_1.0.csv')
-        ser_lam_1 = pd.read_csv(file_path, sep=',')
+        ser_df_10 = read_data('mixed_test_LoRaCNN_snr_-10')
+        ser_df_10.insert(1, '0.25', ser_lam_025)
+        rate_params = ser_df_10.columns[1:]
+        snr_vals = ser_df_10['index']   
 
-        combined_ser = pd.concat([snr_vals, ser_lam_025['0.25'], ser_lam_05['0.5'], ser_lam_07['0.7'], ser_lam_1['1']], axis=1)
-        combined_ser.reset_index(level=0, inplace=True)
-
-        rate_params = combined_ser.columns[2:]
-
-        # Save the results to a .txt file for every rate parameter and create a plot
-        fig, ax = plt.subplots(1, 1, figsize=(8, 6))
-            
+        print(ser_df_6)
+        print(ser_df_10)
+        fig, ax = plt.subplots(1, 1, figsize=(8, 6))           
         for i, rate_param in enumerate(rate_params):
             
-            # Mixed test results
+            # Mixed test results -6 dB
             markers = ['v', 's', 'o', 'D']
             ax.plot(
                 snr_vals,
-                combined_ser[rate_param],
+                ser_df_6[rate_param],
                 marker=markers[i],
-                label=f"SNR=-10 dB, λ={rate_param}",
-                color="black",
+                label=f"Model SNR=-6 dB, λ={rate_param}",
+                color="blue",
             )
+
+            # Mixed test results 10 dB
+            ax.plot(
+                snr_vals,
+                ser_df_10[rate_param],
+                marker=markers[i],
+                label=f"Model SNR=-10 dB, λ={rate_param}",
+                color="red",
+            )
+
         ax.set_yscale("log")
         ax.set_xlabel("SNR [dB]")
         ax.set_ylabel("SER")
         ax.grid(True, which="both", alpha=0.5)
-        ax.set_ylim(1e-6, 1)
+        ax.set_ylim(1e-5, 1)
         ax.set_xlim(-16, -6)
-        ax.legend(loc='lower right')
+        ax.legend(loc='lower left')
 
         plt.show()
         exit()
