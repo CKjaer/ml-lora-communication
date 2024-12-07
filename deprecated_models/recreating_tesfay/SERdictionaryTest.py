2410b695923badd2a46355a59c209acb8d9b0397
Christoffer <clyhol20@student.aau.dk>
2024-12-07 23:34:08 +0100

    Fixed legend text in plots

--- deprecated_models/recreating_tesfay/SERdictionaryTest.py
+++ deprecated_models/recreating_tesfay/SERdictionaryTest.py
@@ -1,5 +1,6 @@
 import matplotlib
-matplotlib.use('TkAgg')  # Change the backend to 'TkAgg' or another interactive backend
+
+matplotlib.use("TkAgg")  # Change the backend to 'TkAgg' or another interactive backend
 import matplotlib.pyplot as plt
 from numpy import savetxt
 import numpy as np
@@ -9,7 +10,6 @@ import os
 import json
 
 
-
 # Load the JSON data
 with open("iq_cnn_output/test_IQCNN/symbol_error_rates.json", "r") as f:
     ser_data = json.load(f)
@@ -17,7 +17,7 @@ with open("iq_cnn_output/test_IQCNN/symbol_error_rates.json", "r") as f:
 # load classical decoder data
 with open("iq_cnn_output/test_IQCNN/classical_ser.json", "r") as f:
     classical_ser_data = json.load(f)
-    
+
 with open("iq_cnn_output/test_IQCNN/cnn_fsd_ser.json", "r") as f:
     fsd_ser_data = json.load(f)
 
@@ -30,84 +30,45 @@ fsd_ser_data = {float(outer_key): {int(inner_key): float(value) for inner_key, v
 if __name__ == "__main__":
     outputpath = "iq_cnn_output/test_IQCNN/plots"
     os.makedirs(outputpath, exist_ok=True)
-    plt.rcParams['mathtext.fontset'] = 'custom'
-    plt.rcParams['mathtext.rm'] = 'Palatino Linotype'
-    plt.rcParams['font.family'] ='Palatino Linotype'
+    plt.rcParams["mathtext.fontset"] = "custom"
+    plt.rcParams["mathtext.rm"] = "Palatino Linotype"
+    plt.rcParams["font.family"] = "Palatino Linotype"
     fs = 20
-    plt.rcParams.update({'font.size': fs})
-        
+    plt.rcParams.update({"font.size": fs})
+
     for rate, values in ser_data.items():
         snr_values = sorted(values.keys())  # Just sort the keys directly
         ser_values = [values[snr] for snr in snr_values]  # loop through to not mix up the order
         classical_ser_values = [classical_ser_data[rate][snr] for snr in snr_values]
         fsd_ser_values = [fsd_ser_data[rate][snr] for snr in snr_values]
-        
+
         print(f"ser_values: {ser_values}")
         print(f"classical_ser_values: {classical_ser_values}")
         print(f"fsd_ser_values: {fsd_ser_values}")
-        
-        #savetxt(os.path.join(outputpath,f'snr_vs_ser_rate_{rate}.csv'), np.array([snr_values, ser_values]).T, delimiter=';', fmt='%d;%.6f')
+
+        # savetxt(os.path.join(outputpath,f'snr_vs_ser_rate_{rate}.csv'), np.array([snr_values, ser_values]).T, delimiter=';', fmt='%d;%.6f')
 
         if rate == 0:
             zero_ser_values = ser_values
             zero_snr_values = snr_values
             zero_classical_ser_values = classical_ser_values
             zero_fsd_ser_values = fsd_ser_values
-        
+
         fig, ax = plt.subplots(1, 1, figsize=(8, 6))
-        ax.plot(
-            zero_snr_values,
-            zero_ser_values,
-            label=f"cnn-tsd λ=0.00",
-            linestyle="dashed",
-            color="red",
-            marker="o"
-        )
-        ax.plot(
-            snr_values,
-            ser_values,
-            marker="o",
-            color="red",
-            label=f"cnn-tsd λ={rate:.2f}"
-        )
-        ax.plot(
-            zero_snr_values,
-            zero_classical_ser_values,
-            label=f"Classical λ=0.00",
-            linestyle="dashed",
-            color="black",
-            marker="v"
-        )
-        ax.plot(
-            snr_values,
-            classical_ser_values,
-            marker="v",
-            color="black",
-            label=f"Classical λ={rate:.2f}"
-        )
-        ax.plot(
-            snr_values,
-            fsd_ser_values,
-            marker="s",
-            color="blue",
-            label=f"cnn-fsd λ={rate:.2f}"
-        )
-        ax.plot(
-            zero_snr_values,
-            zero_fsd_ser_values,
-            label=f"cnn-fsd λ=0.00",
-            linestyle="dashed",
-            color="blue",
-            marker="s"
-        )
-        ax.set_xlabel('SNR [dB]')
-        ax.set_ylabel('SER')
-        ax.set_yscale('log')
+        ax.plot(zero_snr_values, zero_ser_values, label=f"CNN-TSD λ=0.00", linestyle="dashed", color="red", marker="o")
+        ax.plot(snr_values, ser_values, marker="o", color="red", label=f"CNN-TSD λ={rate:.2f}")
+        ax.plot(zero_snr_values, zero_classical_ser_values, label=f"Classical λ=0.00", linestyle="dashed", color="black", marker="v")
+        ax.plot(snr_values, classical_ser_values, marker="v", color="black", label=f"Classical λ={rate:.2f}")
+        ax.plot(snr_values, fsd_ser_values, marker="s", color="blue", label=f"CNN-FSD λ={rate:.2f}")
+        ax.plot(zero_snr_values, zero_fsd_ser_values, label=f"CNN-FSD λ=0.00", linestyle="dashed", color="blue", marker="s")
+        ax.set_xlabel("SNR [dB]")
+        ax.set_ylabel("SER")
+        ax.set_yscale("log")
         ax.set_ylim(1e-5, 1)
         ax.set_xlim(-16, -6)
         ax.grid(True, which="both", alpha=0.5)
-        ax.legend(loc='lower left')
-        
+        ax.legend(loc="lower left")
+
         # if rate != 0:
         #     # Create an inset with the Poisson PMF stem plot
         #     inset_ax = inset_axes(
@@ -136,12 +97,8 @@ if __name__ == "__main__":
         #     # Allow clipping of the stem plot
         #     for artist in stem_inset.get_children():
         #         artist.set_clip_on(False)
-                
-        plt.savefig(
-            os.path.join(outputpath, f"snr_vs_ser_rate_{rate}.pdf"),
-            format = "pdf",
-            bbox_inches = "tight"
-        )
 
-        #plt.tight_layout()
+        plt.savefig(os.path.join(outputpath, f"snr_vs_ser_rate_{rate}.pdf"), format="pdf", bbox_inches="tight")
+
+        # plt.tight_layout()
         plt.show()
