9b61eb9129e808c24f6860e314bf639c372c76fe
xixu64 <mjaure24@student.aau.dk>
2024-12-13 11:38:30 +0100

    Create "Plot_results" folder

---
+++ Plot_results/Plot_singular_SER.py
@@ -0,0 +1,316 @@
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import numpy as np
+import os
+import sys
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+
+SNR = [-6, -8, -10, -12, -14, -16]
+
+SER_classical_0 = [0.000006, 0.001611, 0.037937, 0.203298, 0.468955, 0.698191]
+SER_classical_025=[0.017987, 0.030889, 0.082529, 0.251476, 0.504485, 0.72121]
+SER_classical_05=[0.0346, 0.058052, 0.12541, 0.292114, 0.541924, 0.741722]
+SER_classical_07=[0.049354, 0.083618, 0.153726, 0.243079, 0.487828, 0.703728]
+SER_classical_1=[0.070218, 0.110132, 0.198948,0.381264,0.604959,0.776125 ]
+
+SER_singular_0 = [0.014213, 0.021575, 0.090745, 0.314732, 0.553571, 0.775670]
+SER_singular_025 = [0.027453, 0.042788, 0.121194, 0.343750, 0.591518, 0.750000]
+SER_singular_05 =[0.043139, 0.064844, 0.162861, 0.366071, 0.592634, 0.794643]
+SER_singular_07 =[0.054546, 0.081450, 0.182893, 0.373884, 0.623884, 0.777902]
+SER_singular_1 = [0.073288, 0.106741, 0.213341, 0.444196, 0.674107, 0.822545]
+
+SER_CNN_FSD_0 = [0.00001, 0.0021, 0.0447, 0.2399, 0.5179, 0.7824]
+SER_CNNN_FSD_025 = [0.0073, 0.0154, 0.07, 0.2656, 0.5335, 0.8002]
+SER_CNNN_FSD_05 = [0.014, 0.0277, 0.0897, 0.3147, 0.597,0.827]
+SER_CNNN_FSD_07 = [0.0224, 0.0387, 0.1089, 0.2991, 0.5725, 0.8058]
+SER_CNNN_FSD_1 = [0.0251, 0.0525, 0.1342, 0.3292, 0.6272, 0.8136]
+
+IQ_CNN_0 = [0.000625, 0.0121875, 0.08640625, 0.2803125, 0.5503125, 0.775]
+IQ_CNN_025 = [0.023125, 0.04640625,0.14546875,  0.359375, 0.62703125, 0.82078125]
+IQ_CNN_05 = [0.04609375, 0.08828125,0.2015625,0.435625,0.67828125,0.84484375, ]
+IQ_CNN_07 = [0.0546875, 0.1090625, 0.2465625, 0.47421875,0.71109375, 0.86875]
+IQ_CNN_1 = [0.0815625, 0.145, 0.29578125, 0.52453125, 0.7490625, 0.88]
+plt.rcParams['mathtext.fontset'] = 'custom'
+plt.rcParams['mathtext.rm'] = 'Palatino Linotype'
+plt.rcParams['font.family'] ='Palatino Linotype'
+fs = 20
+plt.rcParams.update({'font.size': fs})
+fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+#FIRST PLOT
+ax.plot(SNR, SER_classical_025, 'k-', marker='^', label='Classical $\lambda=0.25$')
+
+#SER_CNN_FSD should in uncontinous line
+ax.plot(SNR, SER_CNNN_FSD_025, 'b-', marker='v', label='CNN-FSD $\lambda=0.25$')
+
+ax.plot(SNR, IQ_CNN_025, 'r-', marker='o', label='CNN-TSD $\lambda=0.25$')
+
+ax.plot(SNR, SER_singular_025, 'g-',marker='s', label='Unified CNN-FSD $\lambda=0.25$')
+
+#legend down left, write classical lambda=0 and Singular CNN lambda=0
+fs = 18
+plt.rcParams.update({'font.size': fs})
+ax.legend(loc='lower left')
+fs = 20
+plt.rcParams.update({'font.size': fs})
+
+ax.set_yscale("log")
+ax.set_xlabel("SNR [dB]")
+ax.set_ylabel("SER")
+ax.grid(True, which="both", alpha=0.5)
+ax.set_ylim(1e-5, 1)
+# ax.set_xlim(-16, -6)
+ax.set_xlim(-16, -6)
+
+# plt.xlabel('SNR [dB]')
+# plt.ylabel('SER')
+# plt.ylim(1e-5, 1e0)
+# plt.xlim(-16, -6)
+# plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+
+# ax = plt.gca()
+# ax.yaxis.set_major_formatter(ticker.LogFormatterMathtext(base=10))
+
+plt.savefig(os.path.join(current_dir, 'singular_ser_vs_snr_0.25.pdf'))
+# plt.clf()
+fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+#SECONND PLOT
+# plot classical in black with triangle markers and singular in blue with square markers
+ax.plot(SNR, SER_classical_05, 'k-', marker='^', label='Classical $\lambda=0.5$')
+
+#SER_CNN_FSD should in uncontinous line
+ax.plot(SNR, SER_CNNN_FSD_05, 'b-', marker='v', label='CNN-FSD $\lambda=0.5$')
+
+ax.plot(SNR, IQ_CNN_05, 'r-', marker='o', label='CNN-TSD $\lambda=0.5$')
+
+ax.plot(SNR, SER_singular_05, 'g-',marker='s', label='Unified CNN-FSD $\lambda=0.5$')
+
+#legend down left, write classical lambda=0 and Singular CNN lambda=0
+fs = 18
+plt.rcParams.update({'font.size': fs})
+ax.legend(loc='lower left')
+fs = 20
+plt.rcParams.update({'font.size': fs})
+
+ax.set_yscale("log")
+ax.set_xlabel("SNR [dB]")
+ax.set_ylabel("SER")
+ax.grid(True, which="both", alpha=0.5)
+ax.set_ylim(1e-5, 1)
+# ax.set_xlim(-16, -6)
+ax.set_xlim(-16, -6)
+
+plt.savefig(os.path.join(current_dir, 'singular_ser_vs_snr_0.5.pdf'))
+# plt.clf()
+fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+#THIRD PLOT
+# plot classical in black with triangle markers and singular in blue with square markers
+ax.plot(SNR, SER_classical_07, 'k-', marker='^', label='Classical $\lambda=0.7$')
+
+#SER_CNN_FSD should in uncontinous line
+ax.plot(SNR, SER_CNNN_FSD_07, 'b-', marker='v', label='CNN-FSD $\lambda=0.7$')
+
+ax.plot(SNR, IQ_CNN_07, 'r-', marker='o', label='CNN-TSD $\lambda=0.7$')
+
+ax.plot(SNR, SER_singular_07, 'g-',marker='s', label='Unified CNN-FSD $\lambda=0.7$')
+
+#legend down left, write classical lambda=0 and Singular CNN lambda=0
+fs = 18
+plt.rcParams.update({'font.size': fs})
+ax.legend(loc='lower left')
+fs = 20
+plt.rcParams.update({'font.size': fs})
+
+ax.set_yscale("log")
+ax.set_xlabel("SNR [dB]")
+ax.set_ylabel("SER")
+ax.grid(True, which="both", alpha=0.5)
+ax.set_ylim(1e-5, 1)
+# ax.set_xlim(-16, -6)
+ax.set_xlim(-16, -6)
+# plt.legend(loc='lower left')
+
+# plt.xlabel('SNR [dB]')
+# plt.ylabel('SER')
+# plt.ylim(1e-5, 1e0)
+# plt.xlim(-16, -6)
+# plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+
+# ax = plt.gca()
+# ax.yaxis.set_major_formatter(ticker.LogFormatterMathtext(base=10))
+
+plt.savefig(os.path.join(current_dir, 'singular_ser_vs_snr_0.7.pdf'))
+# plt.clf()
+fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+#FOURTH PLOT
+# plot classical in black with triangle markers and singular in blue with square markers
+ax.plot(SNR, SER_classical_1, 'k-', marker='^', label='Classical $\lambda=1.0$')
+
+#SER_CNN_FSD should in uncontinous line
+ax.plot(SNR, SER_CNNN_FSD_1, 'b-', marker='v', label='CNN-FSD $\lambda=1.0$')
+
+ax.plot(SNR, IQ_CNN_1, 'r-', marker='o', label='CNN-TSD $\lambda=1.0$')
+
+ax.plot(SNR, SER_singular_1, 'g-',marker='s', label='Unified CNN-FSD $\lambda=1.0$')
+
+#legend down left, write classical lambda=0 and Singular CNN lambda=0
+fs = 18
+plt.rcParams.update({'font.size': fs})
+ax.legend(loc='lower left')
+fs = 20
+plt.rcParams.update({'font.size': fs})
+
+ax.set_yscale("log")
+ax.set_xlabel("SNR [dB]")
+ax.set_ylabel("SER")
+ax.grid(True, which="both", alpha=0.5)
+ax.set_ylim(1e-5, 1)
+# ax.set_xlim(-16, -6)
+ax.set_xlim(-16, -6)
+# plt.legend(loc='lower left')
+
+# plt.xlabel('SNR [dB]')
+# plt.ylabel('SER')
+# plt.ylim(1e-5, 1e0)
+# plt.xlim(-16, -6)
+# plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+
+# ax = plt.gca()
+# ax.yaxis.set_major_formatter(ticker.LogFormatterMathtext(base=10))
+
+
+
+plt.savefig(os.path.join(current_dir, 'singular_ser_vs_snr_1.0.pdf'))
+
+#Plot for 0 
+#
+fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+ax.plot(SNR, SER_classical_0, 'k-', marker='^', label='Classical $\lambda=0.00$')
+
+#SER_CNN_FSD should in uncontinous line
+ax.plot(SNR, SER_CNN_FSD_0, 'b-', marker='v', label='CNN-FSD $\lambda=0.00$')
+
+ax.plot(SNR, IQ_CNN_0, 'r-', marker='o', label='CNN-TSD $\lambda=0.00$')
+
+ax.plot(SNR, SER_singular_0, 'g-',marker='s', label='Unified CNN-FSD $\lambda=0.00$')
+
+#legend down left, write classical lambda=0 and Singular CNN lambda=0
+fs = 18
+plt.rcParams.update({'font.size': fs})
+ax.legend(loc='lower left')
+fs = 20
+plt.rcParams.update({'font.size': fs})
+
+ax.set_yscale("log")
+ax.set_xlabel("SNR [dB]")
+ax.set_ylabel("SER")
+ax.grid(True, which="both", alpha=0.5)
+ax.set_ylim(1e-5, 1)
+# ax.set_xlim(-16, -6)
+ax.set_xlim(-16, -6)
+# plt.legend(loc='lower left')
+
+# plt.xlabel('SNR [dB]')
+# plt.ylabel('SER')
+# plt.ylim(1e-5, 1e0)
+# plt.xlim(-16, -6)
+# plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+
+# ax = plt.gca()
+# ax.yaxis.set_major_formatter(ticker.LogFormatterMathtext(base=10))
+
+
+
+plt.savefig(os.path.join(current_dir, 'singular_ser_vs_snr_0.0.pdf'))
+
+
+
+#
+fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+ax.plot(SNR, SER_classical_025, 'k-', marker='^', label='Classical $\lambda=0.25$')
+ax.plot(SNR, SER_classical_0, 'k-',linestyle='--', marker='^', label='Classical $\lambda=0.00$')
+
+#SER_CNN_FSD should in uncontinous line
+ax.plot(SNR, SER_CNNN_FSD_025, 'b-', marker='v', label='CNN-FSD $\lambda=0.25$')
+ax.plot(SNR, SER_CNN_FSD_0, 'b-',linestyle='--', marker='v', label='CNN-FSD $\lambda=0.00$')
+
+ax.plot(SNR, IQ_CNN_0, 'r-',linestyle='--', marker='o', label='CNN-TSD $\lambda=0.00$')
+ax.plot(SNR, IQ_CNN_025, 'r-', marker='o', label='CNN-TSD $\lambda=0.25$')
+
+ax.plot(SNR, SER_singular_0, 'g-',linestyle='--',marker='s', label='Unified CNN-FSD $\lambda=0.00$')
+ax.plot(SNR, SER_singular_025, 'g-',marker='s', label='Unified CNN-FSD $\lambda=0.25$')
+
+#legend down left, write classical lambda=0 and Singular CNN lambda=0
+fs = 18
+plt.rcParams.update({'font.size': fs})
+ax.legend(loc='lower left')
+fs = 20
+plt.rcParams.update({'font.size': fs})
+
+ax.set_yscale("log")
+ax.set_xlabel("SNR [dB]")
+ax.set_ylabel("SER")
+ax.grid(True, which="both", alpha=0.5)
+ax.set_ylim(1e-5, 1)
+# ax.set_xlim(-16, -6)
+ax.set_xlim(-16, -6)
+# plt.legend(loc='lower left')
+
+# plt.xlabel('SNR [dB]')
+# plt.ylabel('SER')
+# plt.ylim(1e-5, 1e0)
+# plt.xlim(-16, -6)
+# plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+
+# ax = plt.gca()
+# ax.yaxis.set_major_formatter(ticker.LogFormatterMathtext(base=10))
+
+
+
+plt.savefig(os.path.join(current_dir, 'singular_ser_vs_snr_0.0_and_0.25.pdf'))
+
+#
+
+#
+fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+ax.plot(SNR, SER_classical_1, 'k-', marker='^', label='Classical $\lambda=1.00$')
+ax.plot(SNR, SER_classical_0, 'k-',linestyle='--', marker='^', label='Classical $\lambda=0.00$')
+
+#SER_CNN_FSD should in uncontinous line
+ax.plot(SNR, SER_CNNN_FSD_1, 'b-', marker='v', label='CNN-FSD $\lambda=1.00$')
+ax.plot(SNR, SER_CNN_FSD_0, 'b-',linestyle='--', marker='v', label='CNN-FSD $\lambda=0.00$')
+
+ax.plot(SNR, IQ_CNN_0, 'r-',linestyle='--', marker='o', label='CNN-TSD $\lambda=0.00$')
+ax.plot(SNR, IQ_CNN_1, 'r-', marker='o', label='CNN-TSD $\lambda=1.00$')
+
+ax.plot(SNR, SER_singular_0, 'g-',linestyle='--',marker='s', label='Unified CNN-FSD $\lambda=0.00$')
+ax.plot(SNR, SER_singular_1, 'g-',marker='s', label='Unified CNN-FSD $\lambda=1.00$')
+
+#legend down left, write classical lambda=0 and Singular CNN lambda=0
+fs = 18
+plt.rcParams.update({'font.size': fs})
+ax.legend(loc='lower left')
+fs = 20
+plt.rcParams.update({'font.size': fs})
+
+ax.set_yscale("log")
+ax.set_xlabel("SNR [dB]")
+ax.set_ylabel("SER")
+ax.grid(True, which="both", alpha=0.5)
+ax.set_ylim(1e-5, 1)
+# ax.set_xlim(-16, -6)
+ax.set_xlim(-16, -6)
+# plt.legend(loc='lower left')
+
+# plt.xlabel('SNR [dB]')
+# plt.ylabel('SER')
+# plt.ylim(1e-5, 1e0)
+# plt.xlim(-16, -6)
+# plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+
+# ax = plt.gca()
+# ax.yaxis.set_major_formatter(ticker.LogFormatterMathtext(base=10))
+
+
+
+plt.savefig(os.path.join(current_dir, 'singular_ser_vs_snr_0.0_and_1.00.pdf'))
